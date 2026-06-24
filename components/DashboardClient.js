6cda13677bedfa335956686c4b06cb61484eb912
vekanji-ship-it <vekanji@gmail.com>
2026-06-24 23:48:48 +0800

    Import SUPPLEMENT_LAW_ARTICLES_19 in DashboardClient

--- components/DashboardClient.js
+++ components/DashboardClient.js
@@ -18,6 +18,7 @@ import { SUPPLEMENT_LAW_ARTICLES_15 } from '../lib/supplement-law-data-15'
 import { SUPPLEMENT_LAW_ARTICLES_16 } from '../lib/supplement-law-data-16'
 import { SUPPLEMENT_LAW_ARTICLES_17 } from '../lib/supplement-law-data-17'
 import { SUPPLEMENT_LAW_ARTICLES_18 } from '../lib/supplement-law-data-18'
+import { SUPPLEMENT_LAW_ARTICLES_19 } from '../lib/supplement-law-data-19'
 import { useState, useRef, useEffect } from 'react'
 import { createClient } from '../lib/supabase-client'
 import { CASES, EXAM_QUESTIONS, SOPS } from '../lib/data'
@@ -53,7 +54,8 @@ export default function DashboardClient({ user, isPaid, plan, expiresAt }) {
     SUPPLEMENT_LAW_ARTICLES_15.length +
     SUPPLEMENT_LAW_ARTICLES_16.length +
     SUPPLEMENT_LAW_ARTICLES_17.length +
-    SUPPLEMENT_LAW_ARTICLES_18.length
+    SUPPLEMENT_LAW_ARTICLES_18.length +
+    SUPPLEMENT_LAW_ARTICLES_19.length
 
   // 合併補充資料
   const allCases = [...CASES, ...SUPPLEMENT_CASES]
