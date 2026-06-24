a42e20ab01be88e9b4fe2261bdf817f9967db354
vekanji-ship-it <vekanji@gmail.com>
2026-06-24 23:29:43 +0800

    Add SUPPLEMENT_LAW_ARTICLES_18 to LawSection

--- components/LawSection.js
+++ components/LawSection.js
@@ -18,6 +18,7 @@ import { SUPPLEMENT_LAW_ARTICLES_14 } from '../lib/supplement-law-data-14'
 import { SUPPLEMENT_LAW_ARTICLES_15 } from '../lib/supplement-law-data-15'
 import { SUPPLEMENT_LAW_ARTICLES_16 } from '../lib/supplement-law-data-16'
 import { SUPPLEMENT_LAW_ARTICLES_17 } from '../lib/supplement-law-data-17'
+import { SUPPLEMENT_LAW_ARTICLES_18 } from '../lib/supplement-law-data-18'
 import LawHeatmap from './LawHeatmap'
 import LawRelationMap from './LawRelationMap'
 import Link from 'next/link'
@@ -79,7 +80,8 @@ export default function LawSection({ isPaid }) {
     ...SUPPLEMENT_LAW_ARTICLES_14,
     ...SUPPLEMENT_LAW_ARTICLES_15,
     ...SUPPLEMENT_LAW_ARTICLES_16,
-    ...SUPPLEMENT_LAW_ARTICLES_17
+    ...SUPPLEMENT_LAW_ARTICLES_17,
+    ...SUPPLEMENT_LAW_ARTICLES_18
   ]
 
   const filtered = allArticles.filter(l => {
