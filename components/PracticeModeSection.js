1f48c1dbc8f197cd1117f64588e4a96bbc2e1fae
vekanji-ship-it <vekanji@gmail.com>
2026-06-25 10:35:33 +0800

    Add SUPPLEMENT_LAW_ARTICLES_21 to PracticeModeSection

--- components/PracticeModeSection.js
+++ components/PracticeModeSection.js
@@ -21,6 +21,7 @@ import { SUPPLEMENT_LAW_ARTICLES_17 } from '../lib/supplement-law-data-17'
 import { SUPPLEMENT_LAW_ARTICLES_18 } from '../lib/supplement-law-data-18'
 import { SUPPLEMENT_LAW_ARTICLES_19 } from '../lib/supplement-law-data-19'
 import { SUPPLEMENT_LAW_ARTICLES_20 } from '../lib/supplement-law-data-20'
+import { SUPPLEMENT_LAW_ARTICLES_21 } from '../lib/supplement-law-data-21'
 
 const EXAM_TYPE_MAP = { '共同': 'both', '地政士': 'land_reg', '經紀人': 'broker' }
 
@@ -57,6 +58,7 @@ const RAW_ARTICLES = [
   ...SUPPLEMENT_LAW_ARTICLES_15, ...SUPPLEMENT_LAW_ARTICLES_16,
   ...SUPPLEMENT_LAW_ARTICLES_17, ...SUPPLEMENT_LAW_ARTICLES_18,
   ...SUPPLEMENT_LAW_ARTICLES_19, ...SUPPLEMENT_LAW_ARTICLES_20,
+  ...SUPPLEMENT_LAW_ARTICLES_21,
 ]
 
 const ALL_ARTICLES = RAW_ARTICLES.map(normalizeArticle)
