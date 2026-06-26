2cc6915d13d1128168e4876d7b1083b6dfb6609c
vekanji-ship-it <vekanji@gmail.com>
2026-06-26 21:11:21 +0800

    Import additional law articles in LawSection.js

--- components/LawSection.js
+++ components/LawSection.js
@@ -24,6 +24,9 @@ import { SUPPLEMENT_LAW_ARTICLES_20 } from '../lib/supplement-law-data-20'
 import { SUPPLEMENT_LAW_ARTICLES_21 } from '../lib/supplement-law-data-21'
 import { SUPPLEMENT_LAW_ARTICLES_22 } from '../lib/supplement-law-data-22'
 import { SUPPLEMENT_LAW_ARTICLES_23 } from '../lib/supplement-law-data-23'
+import { SUPPLEMENT_LAW_ARTICLES_24 } from '../lib/supplement-law-data-24'
+import { SUPPLEMENT_LAW_ARTICLES_25 } from '../lib/supplement-law-data-25'
+import { SUPPLEMENT_LAW_ARTICLES_26 } from '../lib/supplement-law-data-26'
 import LawHeatmap from './LawHeatmap'
 import LawRelationMap from './LawRelationMap'
 import Link from 'next/link'
@@ -89,7 +92,8 @@ export default function LawSection({ isPaid }) {
     ...SUPPLEMENT_LAW_ARTICLES_17, ...SUPPLEMENT_LAW_ARTICLES_18,
     ...SUPPLEMENT_LAW_ARTICLES_19, ...SUPPLEMENT_LAW_ARTICLES_20,
     ...SUPPLEMENT_LAW_ARTICLES_21, ...SUPPLEMENT_LAW_ARTICLES_22,
-    ...SUPPLEMENT_LAW_ARTICLES_23,
+    ...SUPPLEMENT_LAW_ARTICLES_23, ...SUPPLEMENT_LAW_ARTICLES_24,
+    ...SUPPLEMENT_LAW_ARTICLES_25, ...SUPPLEMENT_LAW_ARTICLES_26,
   ].map(normalizeArticle), [])
 
   const toggleBookmark = (id) => {
